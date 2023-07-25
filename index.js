bcaa6854e4110168f0db76dfe292c01e7d98a375
ValeGottardello <valengottardello@hotmail.com>
2023-07-25 14:41:31 +1000

    uopdated

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import './css/index.css';
 import App from './App';
 import './css/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
